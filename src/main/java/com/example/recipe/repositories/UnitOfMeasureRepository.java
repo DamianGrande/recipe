6e566bc82a487cfaca38e6311f425f0d5ac6d311
Damian Grande <damian.grande+1-at-980079093348@alimentiamoci.com>
2021-06-28 12:27:49 +0200

    Creating Spring Data Repositories.

---
+++ src/main/java/com/example/recipe/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,7 @@
+package com.example.recipe.repositories;
+
+import com.example.recipe.domain.UnityOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnityOfMeasure, Long> {
+}
