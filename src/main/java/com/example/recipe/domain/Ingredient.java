bc4e64736b11b1233440a70fbcadfd23b00c5629
Damian Grande <damian.grande+1-at-980079093348@alimentiamoci.com>
2021-07-05 09:23:57 +0200

    Complete Refactoring with Project Lombok.

--- src/main/java/com/example/recipe/domain/Ingredient.java
+++ src/main/java/com/example/recipe/domain/Ingredient.java
@@ -1,8 +1,11 @@
 package com.example.recipe.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Data
 @Entity
 public class Ingredient {
     @Id
@@ -17,38 +20,6 @@ public class Ingredient {
 
     @ManyToOne
     private Recipe recipe;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
 
     public UnitOfMeasure getUnityOfMeasure() {
         return unitOfMeasure;
