6e22b2db5f17ce74fd2514aeabc6289d17c51011
Damian Grande <damian.grande+1-at-980079093348@alimentiamoci.com>
2023-08-24 12:50:41 +0200

    Refactor Image Service.

--- src/main/java/com/example/recipe/services/ImageServiceImpl.java
+++ src/main/java/com/example/recipe/services/ImageServiceImpl.java
@@ -1,23 +1,23 @@
 package com.example.recipe.services;
 
 import com.example.recipe.domain.Recipe;
-import com.example.recipe.repositories.RecipeRepository;
+import com.example.recipe.repositories.reactive.RecipeReactiveRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class ImageServiceImpl implements ImageService {
-    private final RecipeRepository recipeRepository;
+    private final RecipeReactiveRepository recipeRepository;
 
     @Autowired
-    public ImageServiceImpl(RecipeRepository recipeRepository) {
+    public ImageServiceImpl(RecipeReactiveRepository recipeRepository) {
         this.recipeRepository = recipeRepository;
     }
 
     @Override
     public void saveImageFile(String recipeId, MultipartFile file) throws Exception {
-        Recipe recipe = this.recipeRepository.findById(recipeId).get();
+        Recipe recipe = this.recipeRepository.findById(recipeId).block();
         Byte[] image = new Byte[file.getBytes().length];
         for (int i = 0; i < file.getBytes().length; i++)
             image[i] = file.getBytes()[i];
--- src/test/java/com/example/recipe/services/ImageServiceImplTest.java
+++ src/test/java/com/example/recipe/services/ImageServiceImplTest.java
@@ -1,7 +1,7 @@
 package com.example.recipe.services;
 
 import com.example.recipe.domain.Recipe;
-import com.example.recipe.repositories.RecipeRepository;
+import com.example.recipe.repositories.reactive.RecipeReactiveRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
@@ -9,22 +9,20 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.Optional;
+import reactor.core.publisher.Mono;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
 class ImageServiceImplTest {
     @Mock
-    RecipeRepository recipeRepository;
+    RecipeReactiveRepository recipeRepository;
 
     ImageService imageService;
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.initMocks(this);
+        MockitoAnnotations.openMocks(this);
         this.imageService = new ImageServiceImpl(recipeRepository);
     }
 
@@ -34,8 +32,7 @@ class ImageServiceImplTest {
         MultipartFile multipartFile = new MockMultipartFile("imageFile", "testing.txt", "text/plain", "Spring Framework Guru".getBytes());
         Recipe recipe = new Recipe();
         recipe.setId(id);
-        Optional<Recipe> recipeOptional = Optional.of(recipe);
-        when(this.recipeRepository.findById(anyString())).thenReturn(recipeOptional);
+        when(this.recipeRepository.findById(anyString())).thenReturn(Mono.just(recipe));
         ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
         this.imageService.saveImageFile(id, multipartFile);
         verify(this.recipeRepository, times(1)).save(argumentCaptor.capture());
